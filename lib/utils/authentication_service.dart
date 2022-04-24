5a79d8c3a4aa618fc07d6b6e186ab44b85eca7cc
Kenneth-29 <kennethmogopodi@gmail.com>
2022-04-24 06:41:59 +0200

    Authentication service created

---
+++ lib/utils/authentication_service.dart
@@ -0,0 +1,85 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Firebase auth instance with user id and email
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+String? uid;
+String? userEmail;
+
+class AuthClient {
+  //Register with email and password
+  Future<User?> register(String name, String email, String password) async {
+    await Firebase.initializeApp();
+    User? user;
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+      await user!.updateDisplayName(name);
+      user = _auth.currentUser;
+
+      if (user != null) {
+        uid = user.uid;
+        userEmail = user.email;
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('An account already exists for the email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return user;
+  }
+
+  // Login method
+  Future<User?> login(String email, String password) async {
+    await Firebase.initializeApp();
+    User? user;
+
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+
+      if (user != null) {
+        uid = user.uid;
+        userEmail = user.email;
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('auth', true);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided.');
+      }
+    }
+
+    return user;
+  }
+
+  // Sign out method
+  Future<String> signOut() async {
+    await _auth.signOut();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('auth', false);
+
+    uid = null;
+    userEmail = null;
+
+    return 'User signed out';
+  }
+}
